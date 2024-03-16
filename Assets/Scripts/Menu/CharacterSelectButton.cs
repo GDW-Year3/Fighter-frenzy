ba30a11fdbfd5ee29af65d93e2bad43179098bd9
Sunu <sunupiousjoseph@gmail.com>
2024-03-16 08:27:53 -0400

    Character Select Scene and Scripts, testing some functionality

---
+++ Assets/Scripts/Menu/CharacterSelectButton.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class CharacterSelectButton : MonoBehaviour
+{
+    [SerializeField] private Image thumbnailImage;
+    //[SerializeField] private GameObject PlayerPedestal;
+    //[SerializeField] private CharacterSelection Characters;
+    //[SerializeField] private GameObject myCharacter;
+    //[SerializeField] private GameObject currentCharacter;
+
+    //[SerializeField] private float Offset;
+
+    
+    public GameObject myCharacter;
+
+    void Start()
+    {
+        //currentCharacter = null;
+    }
+
+    public void Intialize(GameObject newCharacter)
+    {
+        //Texture2D texture = AssetPreview.GetMiniThumbnail(myCharacter);
+        //thumbnailImage.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.width), new Vector2(0.5f,0.5f));
+        myCharacter = newCharacter;
+
+    }
+
+
+    public void OnClick()
+    {
+
+        CharacterSelection.instance.OnLoadCharacter(myCharacter);
+
+        //Instantiate(myCharacter, currentCharacter.transform.position, Quaternion.identity);
+        //Destroy(currentCharacter);
+
+
+        //GameObject currentCharacter;
+        //currentCharacter = myCharacter;
+        //currentCharacter = Instantiate(myCharacter, PlayerPedestal.transform.position + new Vector3(0, Offset, 0), Quaternion.identity) as GameObject;
+        //currentCharacter.SetActive(myCharacter);
+
+
+        // tell character tracker the correct file path
+        //CharacterTracker.instance.setMyCharacter(myCharacter);
+
+        // load the next scene
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/Menu/CharacterSelection.cs
@@ -0,0 +1,144 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TextCore.Text;
+
+public class CharacterSelection : MonoBehaviour
+{
+    [SerializeField] private CharacterSelectButton buttonPrefab;
+    [SerializeField] private Transform contents;
+    //[SerializeField] private GameObject myCharacter;
+
+    //Buttons
+    [SerializeField] private GameObject player1ConfirmButton;
+    [SerializeField] private GameObject player2ConfirmButton;
+    [SerializeField] private GameObject FinalConfirmButton;
+
+
+    [SerializeField] private GameObject currentCharacterPlayer1;
+    [SerializeField] private GameObject currentCharacterPlayer2;
+
+
+
+    [SerializeField] public GameObject[] characters;
+
+    [SerializeField] private int playerNumber;
+
+
+    public static CharacterSelection instance;
+
+    GameObject CharacterPlayer1Showcase;
+    GameObject CharacterPlayer2Showcase;
+
+    List<CharacterSelectButton> characterButtonList = new List<CharacterSelectButton>();
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerNumber = 1;
+        player1ConfirmButton.SetActive(true);
+        player2ConfirmButton.SetActive(false);
+        FinalConfirmButton.SetActive(false);
+
+        CharacterPlayer1Showcase = currentCharacterPlayer1;
+        CharacterPlayer2Showcase = currentCharacterPlayer2;
+
+        for (int i = 0; i < characters.Length; i++)
+        {
+            CharacterSelectButton spawnedPrefab = Instantiate(buttonPrefab, contents);
+            spawnedPrefab.Intialize(characters[i]);
+            characterButtonList.Add(spawnedPrefab);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnLoadCharacter(GameObject currentCharacter)
+    {
+        //Instantiate(myCharacter, currentCharacter.transform.position, Quaternion.identity);
+        //Destroy(currentCharacter);  
+
+        if(currentCharacterPlayer1 != CharacterPlayer1Showcase && playerNumber == 1)
+        {
+            Destroy(currentCharacterPlayer1);
+            currentCharacterPlayer1 = CharacterPlayer1Showcase;
+        }
+        else if (currentCharacterPlayer2 != CharacterPlayer2Showcase && playerNumber == 2)
+        {
+            Destroy(currentCharacterPlayer2);
+            currentCharacterPlayer2 = CharacterPlayer2Showcase;
+        }
+
+        if (playerNumber == 1)
+        {
+            currentCharacterPlayer1 = Instantiate(currentCharacter, CharacterPlayer1Showcase.transform.position, Quaternion.identity);
+            currentCharacterPlayer1.SetActive(true);
+            Debug.Log("Loaded Character");
+        }
+        else if(playerNumber == 2)
+        {
+            currentCharacterPlayer2 = Instantiate(currentCharacter, CharacterPlayer2Showcase.transform.position, Quaternion.identity);
+            currentCharacterPlayer2.SetActive(true);
+            Debug.Log("Loaded Character");
+        }
+
+        //GameObject currentCharacter;
+        //currentCharacter = myCharacter;
+        //currentCharacter = Instantiate(myCharacter, PlayerPedestal.transform.position + new Vector3(0, Offset, 0), Quaternion.identity) as GameObject;
+        //currentCharacter.SetActive(myCharacter);
+    }
+
+    public void OnConfirmPlayer()
+    {
+        if(playerNumber == 1)
+        {
+            player1ConfirmButton.SetActive(false);
+            player2ConfirmButton.SetActive(true);
+
+            playerNumber = 2;
+        }
+        else if(playerNumber == 2)
+        {
+            player2ConfirmButton.SetActive(false);
+            FinalConfirmButton.SetActive(true);
+
+            for (int i = 0; i < characterButtonList.Count; i++)
+            {
+                characterButtonList[i].gameObject.SetActive(false);
+            }
+
+            playerNumber = 3;
+        }
+        else if(playerNumber == 3)
+        {
+            //Confirm players and move on to next scene
+            //currentCharacterPlayer1
+            //currentCharacterPlayer2
+
+            //next scene
+        }
+
+    }
+
+
+
+}
