6b6525cc03ed62b8f4cfb48adbebcd8ef13281a1
AdrianThePadrian <aruggia06@gmail.com>
2024-04-02 17:04:05 -0400

    More Options to options menu
    Added 2 New Volume Sliders
    -UI SFX
    -Music
    
    Added fullscreen and windowed mode to options menu

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -6,9 +6,27 @@ using UnityEngine.Audio;
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
-    public void SetVolume(float volume)
+    public void SetMasterVolume(float volume)
     {
-        audioMixer.SetFloat("volume", volume);
+        audioMixer.SetFloat("Master Volume", volume);
         Debug.Log(volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("Music Volume", volume);
+        Debug.Log(volume);
+    }
+
+    public void SetUIVolume(float volume)
+    {
+        audioMixer.SetFloat("UI Volume", volume);
+        Debug.Log(volume);
+    }
+
+    public void FullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        Debug.Log("Fullscreen is " + isFullscreen);
     }
 }
